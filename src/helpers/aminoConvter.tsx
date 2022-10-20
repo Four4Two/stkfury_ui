bafc61c1a153d824d14579016d6fcfb5eb0340eb
puneetmahajan <puneet@persistence.one>
2022-10-20 11:03:50 +0400

    cleanup for amino converters.

--- src/helpers/aminoConvter.tsx
+++ src/helpers/aminoConvter.tsx
@@ -1,29 +1,32 @@
-import {Coin} from "@cosmjs/proto-signing";
 import {MsgLiquidStake} from "./proto-codecs/codec/pstake/pstake/lscosmos/v1beta1/msgs";
-import {AminoMsg} from "@cosmjs/amino";
+import {AminoMsg, Coin} from "@cosmjs/amino";
+import {AminoConverters} from "@cosmjs/stargate";
 
-export interface AminoMsgSend extends AminoMsg {
+export interface AminoMsgLiquidStake extends AminoMsg {
     readonly type: "cosmos/MsgLiquidStake";
     readonly value: {
         readonly delegator_address: string;
-        readonly amount: Coin;
+        readonly amount?: Coin;
     };
 }
 
-export const customAminoTypes = {
-    "/pstake.lscosmos.v1beta1.MsgLiquidStake": {
-        aminoType: "cosmos/MsgLiquidStake",
-        toAmino:({delegatorAddress, amount}: MsgLiquidStake) => {
-            return {
+
+export function isAminoMsgLiquidStake(msg: AminoMsg): msg is AminoMsgLiquidStake {
+    return msg.type === "cosmos/MsgLiquidStake";
+}
+
+export function createLSCosmosAminoConverters(): AminoConverters {
+    return {
+        "/pstake.lscosmos.v1beta1.MsgLiquidStake": {
+            aminoType: "cosmos/MsgLiquidStake",
+            toAmino: ({delegatorAddress, amount}: MsgLiquidStake): AminoMsgLiquidStake["value"] => ({
                 delegator_address: delegatorAddress,
-                amount
-            };
-        },
-        fromAmino: ({delegator_address, amount}: AminoMsgSend["value"]) => {
-            return {
+                amount: amount,
+            }),
+            fromAmino: ({delegator_address, amount}: AminoMsgLiquidStake["value"]): MsgLiquidStake => ({
                 delegatorAddress: delegator_address,
-                amount
-            };
+                amount: amount,
+            }),
         },
     }
 }
