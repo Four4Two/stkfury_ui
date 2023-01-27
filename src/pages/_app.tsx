ed17f0f88df4866193a4aac18854f7e18cdfb5a7
blackpanther1881 <raju@persistence.one>
2023-01-27 10:59:35 +0400

    update sentry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,8 +22,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-    integrations: [new Integrations.BrowserTracing()],
+    replaysSessionSampleRate: 0.1,
 
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
+
+    integrations: [new Integrations.BrowserTracing(), new Sentry.Replay()],
     tracesSampleRate: 1.0 //lower the value in production
   });
 
